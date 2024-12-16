65e5ec7617357bb125a943669f22f14c454ecc7d
Daniel <daniel@thedaniweb.eu.org>
2024-12-15 21:13:27 -0500

    changed hosting to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,43 +7,30 @@ import playformCompress from "@playform/compress";
 
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
-
-import { visualizer } from "rollup-plugin-visualizer";
-
 import alpinejs from "@astrojs/alpinejs";
 
 import react from "@astrojs/react";
+
 import markdoc from "@astrojs/markdoc";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  site: "https://dantescur.github.io",
-  base: "shame-jcce",
 
   integrations: [
     sitemap(),
     tailwind(),
-    alpinejs({ entrypoint: "./src/lib/entrypoint.ts" }),
-    playformCompress(),
     react(),
     markdoc(),
+    alpinejs({ entrypoint: "./src/lib/entrypoint.ts" }),
+    playformCompress(),
   ],
 
   experimental: {
     responsiveImages: true,
   },
 
-  adapter: node({
-    mode: "standalone",
-  }),
-  vite: {
-    plugins: [
-      visualizer({
-        emitFile: true,
-        filename: "stats.html",
-      }),
-    ],
-  },
+  adapter: netlify(),
 });
